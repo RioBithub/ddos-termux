68fab7d27eeb5872e6b040fb0451368f1f4268ff
RioBithub <30948099+RioBithub@users.noreply.github.com>
2017-11-30 19:24:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-cp ddos ddoser /data/data/com.termux/files/usr/bin;
+cd source && cp ddos ddoser /data/data/com.termux/files/usr/bin;
 cd /data/data/com.termux/files/usr/bin && chmod 777 *;
 echo Your script is ready,type ddos to start
